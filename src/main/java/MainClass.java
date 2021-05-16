7d1650b03d33313899bb41588a72e223b9b75fd9
merlin <oymerlinyo@gmail.com>
2021-05-16 18:12:57 +0300

    Выполненное задание

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,16 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
-    public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+    public static final int CARS_COUNT = 5;
+    public static CyclicBarrier start = new CyclicBarrier(CARS_COUNT);
+    public static CountDownLatch startCars = new CountDownLatch(CARS_COUNT);
+    public static CountDownLatch finishCars = new CountDownLatch(CARS_COUNT);
+    public static Semaphore tunnel = new Semaphore(CARS_COUNT / 2);
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-        Car[] cars = new Car[CARS_COUNT];
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < CARS_COUNT; i++) {
+            cars.add(new Car(race, 20 + new Random().nextInt(10)));
         }
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
-        }
+        cars.forEach(car -> new Thread(car).start());
+        startCars.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        finishCars.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
--- src/main/java/Tunnel.java
+++ src/main/java/Tunnel.java
@@ -3,16 +3,19 @@ public class Tunnel extends Stage {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
     }
+
     @Override
     public void go(Car c) {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                MainClass.tunnel.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
+                MainClass.tunnel.release();
                 System.out.println(c.getName() + " закончил этап: " + description);
             }
         } catch (Exception e) {
